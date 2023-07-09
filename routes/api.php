4d6bfc5ed957f48c86e0203fb672e5038cdc6d93
Amogh Harish <amogh@aharish.xyz>
2023-07-09 18:50:29 +0530

    Added API route for login

--- routes/api.php
+++ routes/api.php
@@ -19,6 +19,8 @@ use App\Http\Controllers\ProjectController;
 
 Route::post('/register/', [AuthController::class, 'createUser']);
 
+Route::post('/login/', [AuthController::class, 'loginUser']);
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
