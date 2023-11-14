aa38ef0e67a0eca769cda366609f3aa4824acbca
Amogh Harish <amogh@aharish.xyz>
2023-11-14 23:01:33 +0530

    Fixed syntax error and added contecxt for TaskReminder

--- app/Http/Controllers/PushNotificationsController.php
+++ app/Http/Controllers/PushNotificationsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Notification;
+use App\Notifications\TaskReminder;
 
 class PushNotificationsController extends Controller
 {
@@ -22,6 +23,6 @@ class PushNotificationsController extends Controller
     }
 
     public function send(Request $request) {
-        Notification::send(auth()->user(), new TaskReminder)
+        Notification::send(auth()->user(), new TaskReminder);
     }
 }
