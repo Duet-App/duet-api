76f57826a77206f1ed70407e6a6755d388223bc1
Amogh Harish <amogh@aharish.xyz>
2024-05-02 01:49:07 +0530

    Update project when notes are updated

--- app/Models/Note.php
+++ app/Models/Note.php
@@ -10,6 +10,7 @@ class Note extends Model
     use HasFactory;
 
     protected $guarded = [];
+    protected $touches = ['project'];
 
     public function project() {
         return $this->belongsTo(Project::class);
